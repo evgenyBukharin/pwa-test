7728501b3424084b1555d91c806e10200e1e2b96
Evgeniy Bukharin <87270911+evgenyBukharin@users.noreply.github.com>
2023-11-27 00:44:26 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -45,3 +45,29 @@ async function networkFirst(request) {
         return cached ?? (await caches.match('/offline.html'));
     }
 }
+
+function isPushSupported() {
+    //checks if user has granted permission to Push notifications
+    if (Notification.permission === 'denied') {
+      alert('User has blocked push notification.');
+      return;
+    }
+
+    //Checks if current browser supports Push notification
+    if (!('PushManager' in window)) {
+      alert('Sorry, Push notification isn\'t supported in your browser.');
+      return;
+    }
+
+    //Get `push notification` subscription id
+
+    //If `serviceWorker` is registered and ready
+    navigator.serviceWorker.ready
+      .then(function (registration) {
+        registration.pushManager.getSubscription()
+        .catch(function (error) {
+          console.error('Error occurred while enabling push ', error);
+        });
+      });
+  }
+isPushSupported()

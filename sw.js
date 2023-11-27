6d1136ec93ab29d391d18e8f75faca273413f3d0
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-11-27 09:18:05 +0500

    asd

--- sw.js
+++ sw.js
@@ -45,3 +45,43 @@ async function networkFirst(request) {
         return cached ?? (await caches.match('/offline.html'));
     }
 }
+
+function subscribePush() {
+    //Subscribes user to Push notifications
+    registration.pushManager
+        .subscribe({
+            userVisibleOnly: true, //Set user to see every notification
+        })
+        .then(function (subscription) {
+            toast('Subscribed successfully.');
+            console.info('Push notification subscribed.');
+            console.log(subscription);
+        })
+        .catch(function (error) {
+            console.error('Push notification subscription error: ', error);
+        });
+}
+
+function isPushSupported() {
+    //checks if user has granted permission to Push notifications
+    if (Notification.permission === 'denied') {
+        alert('User has blocked push notification.');
+        return;
+    }
+
+    //Checks if current browser supports Push notification
+    if (!('PushManager' in window)) {
+        alert("Sorry, Push notification isn't supported in your browser.");
+        return;
+    }
+
+    //Get `push notification` subscription id
+
+    //If `serviceWorker` is registered and ready
+    navigator.serviceWorker.ready.then(function (registration) {
+        registration.pushManager.getSubscription().catch(function (error) {
+            console.error('Error occurred while enabling push ', error);
+        });
+    });
+}
+subscribePush();

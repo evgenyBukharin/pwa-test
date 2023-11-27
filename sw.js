4761cdd357e388d12194e10c36b4a4aec5245177
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-11-27 09:23:10 +0500

    main

--- sw.js
+++ sw.js
@@ -46,42 +46,42 @@ async function networkFirst(request) {
     }
 }
 
-function subscribePush() {
-    //Subscribes user to Push notifications
-    registration.pushManager
-        .subscribe({
-            userVisibleOnly: true, //Set user to see every notification
-        })
-        .then(function (subscription) {
-            toast('Subscribed successfully.');
-            console.info('Push notification subscribed.');
-            console.log(subscription);
-        })
-        .catch(function (error) {
-            console.error('Push notification subscription error: ', error);
-        });
-}
+// function subscribePush() {
+//     //Subscribes user to Push notifications
+//     registration.pushManager
+//         .subscribe({
+//             userVisibleOnly: true, //Set user to see every notification
+//         })
+//         .then(function (subscription) {
+//             toast('Subscribed successfully.');
+//             console.info('Push notification subscribed.');
+//             console.log(subscription);
+//         })
+//         .catch(function (error) {
+//             console.error('Push notification subscription error: ', error);
+//         });
+// }
 
-function isPushSupported() {
-    //checks if user has granted permission to Push notifications
-    if (Notification.permission === 'denied') {
-        alert('User has blocked push notification.');
-        return;
-    }
+// function isPushSupported() {
+//     //checks if user has granted permission to Push notifications
+//     if (Notification.permission === 'denied') {
+//         alert('User has blocked push notification.');
+//         return;
+//     }
 
-    //Checks if current browser supports Push notification
-    if (!('PushManager' in window)) {
-        alert("Sorry, Push notification isn't supported in your browser.");
-        return;
-    }
+//     //Checks if current browser supports Push notification
+//     if (!('PushManager' in window)) {
+//         alert("Sorry, Push notification isn't supported in your browser.");
+//         return;
+//     }
 
-    //Get `push notification` subscription id
+//     //Get `push notification` subscription id
 
-    //If `serviceWorker` is registered and ready
-    navigator.serviceWorker.ready.then(function (registration) {
-        registration.pushManager.getSubscription().catch(function (error) {
-            console.error('Error occurred while enabling push ', error);
-        });
-    });
-}
-subscribePush();
+//     //If `serviceWorker` is registered and ready
+//     navigator.serviceWorker.ready.then(function (registration) {
+//         registration.pushManager.getSubscription().catch(function (error) {
+//             console.error('Error occurred while enabling push ', error);
+//         });
+//     });
+// }
+// subscribePush();
